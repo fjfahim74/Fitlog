ca4085e8cbed02f5bb7ce615d548d2d230d30d54
FJ Fahim <fjfahim8@gmail.com>
2026-09-24 22:47:21 +0600

    Added Context File

---
+++ src/context/WorkoutContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Workout } from "@/types/Workout";
+import React, { createContext, ReactNode, useState } from "react";
+
+interface IWorkoutContext {
+    planWorkouts: Workout[];
+    setPlanWorkouts: React.Dispatch<React.SetStateAction<Workout[]>>;
+    savedWorkouts: Workout[];
+    setSavedWorkouts: React.Dispatch<React.SetStateAction<Workout[]>>;
+}
+
+const WorkoutContext = createContext<IWorkoutContext>({
+    planWorkouts: [],
+    setPlanWorkouts: () => { },
+    savedWorkouts: [],
+    setSavedWorkouts: () => { },
+});
+
+const WorkoutProvider = ({ children }: { children: ReactNode }) => {
+    const [planWorkouts, setPlanWorkouts] = useState<Workout[]>([]);
+    const [savedWorkouts, setSavedWorkouts] = useState<Workout[]>([]);
+
+    const sharedData = {
+        planWorkouts,
+        setPlanWorkouts,
+        savedWorkouts,
+        setSavedWorkouts,
+    };
+
+    return (
+        <WorkoutContext.Provider value={sharedData}>
+            {children}
+        </WorkoutContext.Provider>
+    );
+
+};
+
+export default WorkoutProvider;
